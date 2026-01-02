2d64e08bf27e367e6488056c036a132ab743b2b5
Aarti Jha <aarti_ua2504cdh01@iitp.ac.in>
2026-01-02 21:40:27 +0530

    basics

--- main.py
+++ main.py
@@ -1,1 +1,65 @@
 print ("Hello world")
+print("namastey youtube we are learning python")
+#comments
+#hello i am aarti how are you
+"""hello this is multiline
+aarti
+jha
+carmel"""
+#variable
+sher = "harsh bhaiya"
+a = 12
+#naming convention
+SheryiansSchool = "students" #pascal case
+sheryiansSchool = "students" #camel case
+sheriyans_school = "students" #snake case
+
+#integer
+a = 12
+print(type (a))
+#float
+b = 56.8
+c = 12/3
+
+print(type(b))
+print (type(c))
+#if number is in decimal or in the form of p/q then it will be float
+
+#complex
+v = 24j
+print (type(v))
+
+#tring
+#anything that are available in our keyboard is called string
+
+st = '123445566765 dsgsrehddtdf &*5/#@!$'
+print (type(st))
+
+#boolean
+
+#data type which will and always give the result true and false
+
+b = True
+
+a = False
+
+print(type(b))
+print(type(a))
+
+"""String"""
+
+a = "A"
+print(ord(a))
+
+a = 65
+print (chr(a))
+
+#string indexing
+a = "SHER"
+print(a[1])
+print(a[1],a[-3])
+
+#string slicing
+
+a = "SHER CODER"
+print(a[0:4:1])
