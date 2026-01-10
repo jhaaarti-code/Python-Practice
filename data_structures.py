b969e458249b8284cef8a5bbd6eecaac8dc50549
Aarti Jha <aarti_ua2504cdh01@iitp.ac.in>
2026-01-10 22:38:42 +0530

    Start Data Structures practice

---
+++ data_structures.py
@@ -0,0 +1,78 @@
+# Data structures - storing multiple values in one variable
+# 4 types of in build data structures
+# List
+# Tuple
+# Dictionary
+# Set
+
+# Lists are -:
+# Mutable - object's value can be changed after creation
+# Duplicates - means same value occuring multiple times. Lists allows this
+# Ordered - This means you can access elements using their position(index)
+# Hetrogeneous - Lists have heterogeneous nature that means we can have multiple data type inside the list
+
+# List syntax - we have to use square bracket
+
+a = [12,13,14,16,19,34.5,True,print()]
+
+# List indexing
+#print(a[0])
+# print(a[1])
+
+# List slicing
+# print(a[0:5:1])
+# print(a[-2])
+
+# List Traversing
+
+# 1st way using index
+
+# for i in range (len(a)):
+#     print(a[i])
+
+# 2nd way directly on values
+
+# for i in a:
+#     print(i)
+
+# List methods
+
+# print(dir(list))
+
+# help(list)
+
+# l = [1,2,3,4,5]
+
+# l.append(6)
+# l.append(7)
+
+# print(l)
+
+# l = [1,3,4,5]
+
+# l.insert(1,2)
+
+# print(l)
+
+# l = [1,3,4,5]
+
+# l.extend([8,10,15])
+
+# print(l)
+
+# l = [1,2,3,4,5]
+# l.remove(2)
+# print(l)
+
+# l = [1,2,3,2,4,5]
+# l.remove(2)
+# print(l)
+
+# l = [1,2,3,4,5]
+# l[0] = 10
+# print(l)
+
+
+
+
+
