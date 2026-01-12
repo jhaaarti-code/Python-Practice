408aa302c88221a37ccaf87e32a72464c5aadfc4
Aarti Jha <aarti_ua2504cdh01@iitp.ac.in>
2026-01-12 13:31:13 +0530

    Add dictionary frequency and merge solutions

---
+++ exception_handling.py
@@ -0,0 +1,67 @@
+# a = int(input("tell you number :- "))
+
+# print(10/a)
+
+# print("ok i have done the divison")
+
+# a = int(input("tell you number :- "))
+
+# try:
+#     print(10/a)
+    
+# except ZeroDivisionError:
+#     print("Sorry you cannot divide by 0")
+  
+
+# print("ok i have done the divison")
+
+# a = int(input("tell you number :- "))
+
+# try:
+#     print(10/a)
+    
+# except Exception as err:
+#     print(f"Sorry there is an err {err}")
+  
+
+# print("ok i have done the divison")
+
+# a = int(input("tell you number :- "))
+
+# try:
+#     print(10/a)
+    
+# except Exception as err:
+#     print(f"Sorry there is an err {err}")
+    
+# else:
+#     print("good there is no exception")
+    
+# finally:
+#     print("i will run no matter what")
+  
+
+# print("ok i have done the divison")
+
+# age = int(input("tell your age -: "))
+
+# try:
+
+#    if age < 10 or age > 18:
+#     raise valueError("your age must be between 10 and 18")
+
+#    else: 
+#     print("welcome to the club")
+    
+# except Exception as err:
+#     print(f"an error occured as {err}")
+    
+    
+    
+# print("the club will start soon")
+
+
+
+
+    
+
